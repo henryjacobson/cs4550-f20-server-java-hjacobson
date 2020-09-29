e083c2ab16b76710d4465e4edbc906498e502b57
Henry <henryajacobson@gmail.com>
2020-09-29 14:18:34 -0500

    removed more and reorganized

--- src/main/resources/static/user-admin/user-admin.controller.client.js
+++ src/main/resources/static/user-admin/user-admin.controller.client.js
@@ -16,44 +16,6 @@ const deleteUser = (index) => {
     })
 }
 
-let selectedUserIndex = -1
-const selectUser = (index) => {
-  selectedUserIndex = index
-  $("#usernameFld").val(users[index].username)
-}
-
-let $template
-let tbody
-
-function renderUsers(users) {
-  tbody.empty()
-  for(let i=0; i<users.length; i++) {
-    const user = users[i]
-    const username = user.username
-    const fName = user.first
-    const lName = user.last
-    const role = user.role
-
-    const $clone = $template.clone()
-
-    $clone.removeClass("wbdv-hidden")
-
-    const $username = $clone.find(".wbdv-username")
-    $username.html(username)
-    const $firstName = $clone.find(".wbdv-first-name")
-    $firstName.html(fName)
-    const $lastName = $clone.find(".wbdv-last-name")
-    $lastName.html(lName)
-    const $removeBtn = $clone.find(".wbdv-remove")
-    $removeBtn.click(() => deleteUser(i))
-    $clone
-      .find(".wbdv-select")
-      .click(() => selectUser(i))
-
-    tbody.append($clone)
-  }
-}
-
 const createUser = () => {
   const username = $("#usernameFld").val()
   const first = $("#firstNameFld").val()
@@ -81,6 +43,38 @@ const updateUser = () => {
       users[selectedUserIndex] = updatedFields
       renderUsers(users)
     })
+}
+
+let selectedUserIndex = -1
+const selectUser = (index) => {
+  selectedUserIndex = index
+  $("#usernameFld").val(users[index].username)
+}
+
+let $template
+let tbody
+
+function renderUsers(users) {
+  tbody.empty()
+  for(let i=0; i<users.length; i++) {
+    const $clone = $template.clone()
+
+    $clone.removeClass("wbdv-hidden")
+
+    const user = users[i]
+    $clone.find(".wbdv-username").html(user.username)
+    $clone.find(".wbdv-first-name").html(user.first)
+    $clone.find(".wbdv-last-name").html(user.last)
+    $clone.find(".wbdv-role").html(user.role)
+
+    const $removeBtn = $clone.find(".wbdv-remove")
+    $removeBtn.click(() => deleteUser(i))
+    $clone
+      .find(".wbdv-select")
+      .click(() => selectUser(i))
+
+    tbody.append($clone)
+  }
 }
 
 function init() {
